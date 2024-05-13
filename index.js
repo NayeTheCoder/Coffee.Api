9ede5fa61c9513798d3df04edced5b1543ff8336
Nayeli <nayeli585052@gmail.com>
2024-05-13 18:37:43 -0500

    Added bootstrap to API

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+fetch('https://api.sampleapis.com/coffee/hot')
+.then(response => response.json())
+.then(data => {console.log(data);
+hotDrinks(data);
+});
+
 fetch('https://api.sampleapis.com/coffee/iced')
 .then(response => response.json())
-.then(data => console.log(data));
+.then(data => {console.log(data);
+coldDrinks(data);
+});
+
+function hotDrinks(hotdrinks) {
+    const parent = document.getElementById('parent');
+    for ( let i =0; i < hotdrinks.length; i++) {
+        let hotDrink = hotdrinks[i];
+        var item = document.createElement('div');
+        item.classList.add('col');
+        item.innerHTML = `
+        <div class="card">
+            <img src="${hotDrink.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${hotDrink.title}</h5>
+              <p class="card-text"> ${hotDrink.description}</p>
+            </div>
+          </div>
+        `;
+        parent.appendChild(item);
+    }
+}
+function coldDrinks(coldDrinks) {
+    const parent = document.getElementById('parent');
+    for ( let i =0; i < coldDrinks.length; i++) {
+        let coldDrink = coldDrinks[i];
+        var item = document.createElement('div');
+        item.classList.add('col');
+        item.innerHTML = `
+        <div class="card">
+            <img src="${coldDrink.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${coldDrink.title}</h5>
+              <p class="card-text"> ${coldDrink.description}</p>
+            </div>
+          </div>
+        `;
+        parent.appendChild(item);
+    }}
