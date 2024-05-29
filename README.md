af5663ef1a10f5f5d2686b699598a2c85d872674
Nayeli <nayeli585052@gmail.com>
2024-05-28 22:04:35 -0500

    Last changes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Coffee.Api
+
+Coffee API:
+
+Functionality requirements:
+
+1.) Click the green "Code" button. 
+2.) Click on "Download ZIP".
+3.) Extract the file and in your file explorer go into the Coffee.Api folder.
+4.) Double click the index.html file to view the website in your default browser.
--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-fetch('https://api.sampleapis.com/coffee/hot')
-.then(response => response.json())
-.then(data => {console.log(data);
-hotDrinks(data);
-});
 
-fetch('https://api.sampleapis.com/coffee/iced')
-.then(response => response.json())
-.then(data => {console.log(data);
-coldDrinks(data);
-});
 
 function hotDrinks(hotdrinks) {
-    const parent = document.getElementById('parent');
-    for ( let i =0; i < hotdrinks.length; i++) {
-        let hotDrink = hotdrinks[i];
-        var item = document.createElement('div');
-        item.classList.add('col');
-        item.innerHTML = `
-        <div class="card">
-            <img src="${hotDrink.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${hotDrink.title}</h5>
-              <p class="card-text"> ${hotDrink.description}</p>
-            </div>
+
+  const parent = document.getElementById('hot-parent');
+  for ( let i =0; i < hotdrinks.length; i++) {
+      let hotDrink = hotdrinks[i];
+      var item = document.createElement('div');
+      item.classList.add('col');
+      item.innerHTML = `
+      <div class="card">
+          <img src="${hotDrink.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${hotDrink.title}</h5>
+            <p class="card-text"> ${hotDrink.description}</p>
           </div>
-        `;
-        parent.appendChild(item);
-    }
+        </div>
+      `;
+      parent.appendChild(item);
+  }
 }
 function coldDrinks(coldDrinks) {
-    const parent = document.getElementById('parent');
-    for ( let i =0; i < coldDrinks.length; i++) {
-        let coldDrink = coldDrinks[i];
-        var item = document.createElement('div');
-        item.classList.add('col');
-        item.innerHTML = `
-        <div class="card">
-            <img src="${coldDrink.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${coldDrink.title}</h5>
-              <p class="card-text"> ${coldDrink.description}</p>
-            </div>
+  
+  const parent = document.getElementById('iced-parent');
+  for ( let i =0; i < coldDrinks.length; i++) {
+      let coldDrink = coldDrinks[i];
+      var item = document.createElement('div');
+      item.classList.add('col');
+      item.innerHTML = `
+      <div class="card">
+          <img src="${coldDrink.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${coldDrink.title}</h5>
+            <p class="card-text"> ${coldDrink.description}</p>
           </div>
-        `;
-        parent.appendChild(item);
-    }}
+        </div>
+      `;
+      parent.appendChild(item);
+  }}
+
+  if (document.getElementById("iced-parent") != null){
+    fetch('https://api.sampleapis.com/coffee/iced')
+    .then(response => response.json())
+    .then(data => {console.log(data);
+    coldDrinks(data);
+  });
+  }
+  else if (document.getElementById("hot-parent") != null){
+    fetch('https://api.sampleapis.com/coffee/hot')
+  .then(response => response.json())
+  .then(data => { 
+  console.log(data);
+  hotDrinks(data);
+  });
+  }
+
+  else {
+    
+  }
